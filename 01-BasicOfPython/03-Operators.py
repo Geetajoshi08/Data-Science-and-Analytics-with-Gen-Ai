2f36c0a0d99c8fe6c52afe2412d38932f6ebe30e
Geetajoshi08 <geetajoshi9621@gmail.com>
2026-06-13 18:27:43 +0530

    String and operators completed

---
+++ 01-BasicOfPython/03-Operators.py
@@ -0,0 +1,11 @@
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter your second number : "))
+
+print(f"The sum of {num1} and {num2} is : {num1 + num2}")
+print(f"The difference of {num1} and {num2} is : {num1 - num2}")
+print(f"The product of {num1} and {num2} is : {num1 * num2}")
+print(f"The quotient of {num1} and {num2} is : {num1 / num2}")
+print(f"The modulus of {num1} and {num2} is : {num1 % num2}")
+print(f"The power of {num1} and {num2} is : {num1 ** num2}")
+print(f"The floor division of {num1} and {num2} is : {num1 // num2}")
